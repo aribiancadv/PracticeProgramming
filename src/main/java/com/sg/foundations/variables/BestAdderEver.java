934e7674e5475e87127f80286417c17cb9ea6857
Ari Bianca DV <103449903+aribiancadv@users.noreply.github.com>
2022-08-08 15:33:43 +0100

    Added java classes for practice

---
+++ src/main/java/com/sg/foundations/variables/BestAdderEver.java
@@ -0,0 +1,20 @@
+package com.sg.foundations.variables;
+
+public class BestAdderEver {
+    public static void main(String[] args) {
+
+        double firstvar;
+        double secondvar;
+        double thirdvar;
+        double result;
+
+        firstvar = 1.0;
+        secondvar = 3.14;
+        thirdvar = 5.0;
+
+        result = firstvar + secondvar + thirdvar;
+
+        System.out.println(firstvar + " + " + secondvar + " + " + thirdvar + " = " + result);
+
+    }
+}
